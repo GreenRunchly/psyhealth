659e732e2bad138b556d832796f0059da69a61f0
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2022-11-14 14:46:06 +0700

    Update

--- AndroidApp/app/src/main/assets/js/page-script.js
+++ AndroidApp/app/src/main/assets/js/page-script.js
@@ -39,3 +39,16 @@ $(document).on('input','.slider-input',function(e){
     sliderChange(); 
 });
 sliderChange();
+/* Buttons Action */
+$(document).on('click','.toggle',function(e){
+    var idnya = $(this).attr("toggle-id");
+    var has = $("#"+idnya).hasClass("on");         
+
+    if (has == true){
+        $("#"+idnya).addClass("off");
+        $("#"+idnya).removeClass("on");
+    }else{
+        $("#"+idnya).addClass("on");
+        $("#"+idnya).removeClass("off");
+    }
+});
